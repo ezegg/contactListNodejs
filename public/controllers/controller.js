eae1322bddc77e1500746cfae9b375fe0bc7257c
Ezequiel García <ezeezegg@gmail.com>
2015-12-01 19:18:41 -0600

    harcode values in Appctrl and show in frontend

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,3 +1,26 @@
-function AppCtrl() {
-	console.log('hello controller')
+function AppCtrl($scope) {
+	console.log('hello controller');
+
+	person1 = {
+		name: 'Ezequiel',
+		email: 'ezeezegg@gmail.com',
+		number: '1111-1111'
+	};
+
+	person2 = {
+		name: 'sebastian',
+		email: 'sebastian@gmail.com',
+		number: '2222-1111'
+	};
+
+	person3 = {
+		name: 'jorge',
+		email: 'jorge@gmail.com',
+		number: '3333-1111'
+	};
+
+	var contactlist = [person1, person2, person3];
+
+	$scope.contactlist = contactlist;
+
 }
