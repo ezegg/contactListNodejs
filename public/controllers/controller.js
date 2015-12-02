f0a891821316e6764008c5b94baeb0a272934585
Ezequiel García <ezeezegg@gmail.com>
2015-12-01 22:55:32 -0600

    create function refresh

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,16 +1,21 @@
 function AppCtrl($scope, $http) {
 	console.log('hello controller');
 
-	$http.get('/contactlist').success(function  (response) {
-		console.log('got the data i requested');
-		console.log(response)
-		$scope.contactlist = response;
-	});
+	var refresh = function  () {
+		$http.get('/contactlist').success(function  (response) {
+			console.log('got the data i requested');
+			console.log(response)
+			$scope.contactlist = response;
+		});
+	};
+
+	refresh();
 
 	$scope.addContact = function  () {
 		console.log($scope.contact);
 		$http.post('/contactlist', $scope.contact).success(function  (response) {
 			console.log(response);
+			refresh();
 		})
 	}
 
